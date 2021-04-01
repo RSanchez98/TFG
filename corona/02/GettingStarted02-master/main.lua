253b36a73ebf05c116ff668b0d40d103ca8c2a24
RSanchez98 <rsanchez98rsv@gmail.com>
2021-04-01 17:48:20 +0200

    background y nave

---
+++ corona/02/GettingStarted02-master/main.lua
@@ -0,0 +1,92 @@
+-- Copyright (c) 2017 Corona Labs Inc.
+-- Code is MIT licensed and can be re-used; see https://www.coronalabs.com/links/code/license
+-- Other assets are licensed by their creators:
+--    Art assets by Kenney: http://kenney.nl/assets
+--    Music and sound effect assets by Eric Matyas: http://www.soundimage.org
+
+local physics = require( "physics" )
+physics.start()
+physics.setGravity( 0, 0 )
+
+-- Seed the random number generator
+math.randomseed( os.time() )
+
+-- Configure image sheet
+local sheetOptions =
+{
+    frames =
+    {
+        {   -- 1) asteroid 1
+            x = 0,
+            y = 0,
+            width = 102,
+            height = 85
+        },
+        {   -- 2) asteroid 2
+            x = 0,
+            y = 85,
+            width = 90,
+            height = 83
+        },
+        {   -- 3) asteroid 3
+            x = 0,
+            y = 168,
+            width = 100,
+            height = 97
+        },
+        {   -- 4) ship
+            x = 0,
+            y = 265,
+            width = 98,
+            height = 79
+        },
+        {   -- 5) laser
+            x = 98,
+            y = 265,
+            width = 14,
+            height = 40
+        },
+    },
+}
+local objectSheet = graphics.newImageSheet( "gameObjects.png", sheetOptions )
+
+-- Initialize variables
+local lives = 3
+local score = 0
+local died = false
+
+local asteroidsTable = {}
+
+local ship
+local gameLoopTimer
+local livesText
+local scoreText
+
+-- Set up display groups
+local backGroup = display.newGroup()  -- Display group for the background image
+local mainGroup = display.newGroup()  -- Display group for the ship, asteroids, lasers, etc.
+local uiGroup = display.newGroup()    -- Display group for UI objects like the score
+
+-- Load the background
+local background = display.newImageRect( backGroup, "background.png", 800, 1400 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+ship = display.newImageRect( mainGroup, objectSheet, 4, 98, 79 )
+ship.x = display.contentCenterX
+ship.y = display.contentHeight - 100
+physics.addBody( ship, { radius=30, isSensor=true } )
+ship.myName = "ship"
+
+-- Display lives and score
+livesText = display.newText( uiGroup, "Lives: " .. lives, 200, 80, native.systemFont, 36 )
+scoreText = display.newText( uiGroup, "Score: " .. score, 400, 80, native.systemFont, 36 )
+
+-- Hide the status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+
+local function updateText()
+	livesText.text = "Lives: " .. lives
+	scoreText.text = "Score: " .. score
+end
--- corona/02/StarExplorer/main.lua
+++ corona/02/StarExplorer/main.lua
@@ -4,4 +4,78 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+--FÍSICAS Y GRAVEDAD
+local physics = require("physics")
+physics.start()
+physics.setGravity(0,0)
+
+--SEMILLA ALEATORIA
+math.randomseed(os.time())
+
+--CONFIGURACIÓN DE IMAGENES
+local sheetOptions =
+{
+    frames =
+    {
+        { -- asteroide 1
+            x = 0,
+            y = 0,
+            width = 102,
+            height = 85
+        },
+        { -- asteroide 2
+            x = 0,
+            y = 85,
+            width = 90,
+            height = 86
+        },
+        { -- asteroide 3
+            x = 0,
+            y = 168,
+            width = 100,
+            height = 97
+        },
+        { -- nave
+            x = 0,
+            y = 265,
+            width = 98,
+            height = 79
+        },
+        { -- laser
+            x = 98,
+            y = 265,
+            width = 14,
+            height = 40
+        },
+    },
+}
+local objectSheet = graphics.newImageSheet("../GettingStarted02-master/gameObjects.png", sheetOptions)
+
+
+--INICIALIZAR VARIABLES
+local lives = 3
+local score = 0
+local died = false
+
+local asteroidTable ={}
+
+local ship
+local gameLoopTimer
+local livesText
+local scoreText
+
+--GRUPOS DE VISUALIZACIÓN   
+local backGroup = display.newGroup()   --imagen de fondo
+local mainGroup = display.newGroup()   --nave, asteroides y laser
+local uiGroup = display.newGroup()     --UI (puntiación)
+
+--CARGAR EL FONDO
+local background = display.newImageRect(backGroup, "../GettingStarted02-master/background.png", 800, 1400)
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+ship = display.newImageRect( mainGroup, objectSheet, 4, 98, 79 )
+ship.x = display.contentCenterX
+ship.y = display.contentHeight - 100
+physics.addBody( ship, { radius=30, isSensor=true } )
+ship.myName = "ship"
