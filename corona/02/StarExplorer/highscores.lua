d1a710ee5562699d344af928366192e6055c963f
RSanchez98 <rsanchez98rsv@gmail.com>
2021-04-13 15:31:34 +0200

    visualizar puntuaciones mas altas

--- corona/02/StarExplorer/highscores.lua
+++ corona/02/StarExplorer/highscores.lua
@@ -19,3 +19,62 @@ local function loadScores()
 		scoresTable = {10000, 7500, 5200, 4700, 3500, 3200, 1200, 1100, 800, 500}
 	end
 end
+
+--GUARDAR DATOS
+local function savesScores()
+
+	for i = #scoresTable, 11, -1 do
+		table.remove(scoresTable, i)
+	end 
+
+	local file = io.open(filePath,"w")
+
+	if file then
+		file:write(json.encore(scoresTable))
+		io.close(file)
+	end
+	
+	--VISUALIZAR PUNTUACIONES ALTAS
+	loadScores()
+	table.insert(scoresTable,composer.getVariable("finalScore"))
+	composer.setVariable("finalScore",0)
+
+	--ordena de mauyor a menor
+	local function compare(a,b)
+		return a > b
+	end
+	table.sort(scoresTable,compare);
+
+	--una vez ordenada, guarda las puntuaciones
+	savesScores()
+
+	--creamos fondo y texto
+	local background = display.newImageRect(sceneGroup,"background.png",800,1400);
+	background.x = display.contentCenterX
+	background.y = display.contentCenterY
+
+	local highScoresHeader = display.newText(sceneGroup, "High scores", dsiplay.contentCenterX, 100, native.systemFont, 44)
+
+	--mostramos la puntuación de 1 a 10
+	for i = 1, 10 do
+		if(scoresTable[i])then
+			local yPos = 150 + (i*56)
+
+			local rankNum = display.newText( sceneGroup, i .. ")", display.contentCenterX-50, yPos, native.systemFont, 36 )
+            rankNum:setFillColor( 0.8 )
+            rankNum.anchorX = 1
+ 
+            local thisScore = display.newText( sceneGroup, scoresTable[i], display.contentCenterX-30, yPos, native.systemFont, 36 )
+            thisScore.anchorX = 0
+		end
+	end
+
+	--volver al menú
+	locla menuButton = display.newText(sceneGropu, "Menu", display.contentCenterX, 810, native.systemFont, 44)
+	menuButton:setFillColor(0.75, 0.78, 1)
+	menuButton:addEventListener("tap", gotoMenu)
+end
+
+local function gotoMenu()
+    composer.gotoScene( "menu", { time=800, effect="crossFade" } )
+end
