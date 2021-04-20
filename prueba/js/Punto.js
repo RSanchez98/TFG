ae09d2c55534187991f955fd39047af1cd86b73d
RSanchez98 <rsanchez98rsv@gmail.com>
2021-04-20 19:55:12 +0200

    sprites y tiles

---
+++ prueba/js/Punto.js
@@ -0,0 +1,11 @@
+//punto en rejilla de coordenadas
+function Punto (x, y)
+{
+    this.x = x;
+    this.y = y;
+}
+
+Punto.prototype.coincide = function(punto)
+{
+    return ( this. x == punto.x && this.y == punto.y) ? true : false;
+}
---
+++ prueba/js/Sprite.js
@@ -0,0 +1,8 @@
+function Sprite(ruta, idSobreZero, posicionEnHoja)
+{
+    var elementosRuta = ruta.split("/");
+    this.rutaHojaOrigen = "img/" + elementosRuta[elementosRuta.length-1];
+    this.idSobreZero = idSobreZero;
+    this.idSobreUno = idSobreZero + 1;
+    this.posicionEnHoja = posicionEnHoja; 
+}
