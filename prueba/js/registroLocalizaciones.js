734fcd059d7d470718aa32a9f0d3d4f1aff70cf1
RSanchez98 <rsanchez98rsv@gmail.com>
2021-05-10 12:12:18 +0200

    ventana de inventario y modificaciones

--- prueba/js/registroLocalizaciones.js
+++ prueba/js/registroLocalizaciones.js
@@ -4,6 +4,7 @@ var registroLocalizaciones =
     {
         let localizaciones = new Array();
         localizaciones.push(new RegistroLocalizacionEntrada("Ciudad del Arbol Milenario", "niveles/villa48.json", "img/villa48.nivel.png", 0, 630));
+        localizaciones.push(new RegistroLocalizacionEntrada("Pedrusco de la Rascacion", "niveles/mapa1.json", "img/mapa1.nivel.png", 0, 630));
 
         for(var i = 0; i < localizaciones.length; i++) 
         {
