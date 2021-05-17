3fd5ab5a1e8643aa745734947a1a9ce15b3f9d33
RSanchez98 <rsanchez98rsv@gmail.com>
2021-05-17 11:01:48 +0200

    Documentacion --> Capa de tiles (incompleto)

--- prueba/js/CapaMapaTiles.js
+++ prueba/js/CapaMapaTiles.js
@@ -1,4 +1,5 @@
-function CapaMapaTiles(datosCapa, indiceZ, anchoDeLosTiles, altoDeLosTiles, paletasSprites) {
+function CapaMapaTiles(datosCapa, indiceZ, anchoDeLosTiles, altoDeLosTiles, paletasSprites) 
+{
 	this.anchoEnTiles = parseInt(datosCapa.width);
 	this.altoEnTiles = parseInt(datosCapa.height);
 	this.x = parseInt(datosCapa.x);
@@ -6,12 +7,17 @@ function CapaMapaTiles(datosCapa, indiceZ, anchoDeLosTiles, altoDeLosTiles, pale
 	this.z = indiceZ;
 	this.tiles = [];
 
-	for (y = 0; y < this.altoEnTiles; y++) {
-		for (x = 0; x < this.anchoEnTiles; x++) {
+	for (y = 0; y < this.altoEnTiles; y++) 
+	{
+		for (x = 0; x < this.anchoEnTiles; x++) 
+		{
 			var idSpriteActualSobreUno = datosCapa.data[x + y * this.anchoEnTiles];
-			if (idSpriteActualSobreUno == 0) {
+			if (idSpriteActualSobreUno == 0) 
+			{
 				this.tiles.push(null);
-			} else {
+			} 
+			else 
+			{
 				var spriteActual = this.encontrarSpriteEnPaletaPorId(idSpriteActualSobreUno - 1, paletasSprites);
 
 				this.tiles.push(new Tile(x, y, indiceZ, anchoDeLosTiles, altoDeLosTiles, spriteActual));
@@ -20,10 +26,13 @@ function CapaMapaTiles(datosCapa, indiceZ, anchoDeLosTiles, altoDeLosTiles, pale
 	}
 }
 
-CapaMapaTiles.prototype.encontrarSpriteEnPaletaPorId = function(idSpriteSobreZero, paletasSprites) {
-	for (s = 0; s < paletasSprites.length; s++) {
+CapaMapaTiles.prototype.encontrarSpriteEnPaletaPorId = function(idSpriteSobreZero, paletasSprites) 
+{
+	for (s = 0; s < paletasSprites.length; s++) 
+	{
 		if (idSpriteSobreZero >= paletasSprites[s].primerSpriteSobreUno - 1 &&
-			idSpriteSobreZero < paletasSprites[s].totalSprites + paletasSprites[s].primerSpriteSobreUno + 1) {
+			idSpriteSobreZero < paletasSprites[s].totalSprites + paletasSprites[s].primerSpriteSobreUno + 1) 
+			{
 			return paletasSprites[s].sprites[Math.abs(paletasSprites[s].primerSpriteSobreUno - 1 - idSpriteSobreZero)];
 		}
 	}
