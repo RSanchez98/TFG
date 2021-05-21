bbc0657fa1a053e6cec38525337b149dafa8da90
RSanchez98 <rsanchez98rsv@gmail.com>
2021-05-21 12:47:20 +0200

    Documentacion --> Limites y rectángulos

--- juego_web_master/juego-web-master/juego-web/js/mando.js
+++ juego_web_master/juego-web-master/juego-web/js/mando.js
@@ -40,51 +40,51 @@ var mando = {
 		if (mando.botonPulsado(mando.objeto.buttons[0])) {
 			console.log("Mando: A");
 		}
-		// if (mando.botonPulsado(mando.objeto.buttons[1])) {
-		// 	console.log("Mando: B");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[2])) {
-		// 	console.log("Mando: X");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[3])) {
-		// 	console.log("Mando: Y");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[4])) {
-		// 	console.log("Mando: L1");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[5])) {
-		// 	console.log("Mando: R1");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[6])) {
-		// 	console.log("Mando: L2");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[7])) {
-		// 	console.log("Mando: R2");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[8])) {
-		// 	console.log("Mando: SELECT");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[9])) {
-		// 	console.log("Mando: START");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[10])) {
-		// 	console.log("Mando: L3");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[11])) {
-		// 	console.log("Mando: R3");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[12])) {
-		// 	console.log("Mando: Arriba");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[13])) {
-		// 	console.log("Mando: Abajo");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[14])) {
-		// 	console.log("Mando: Izquierda");
-		// }
-		// if (mando.botonPulsado(mando.objeto.buttons[15])) {
-		// 	console.log("Mando: Derecha");
-		// }
+		if (mando.botonPulsado(mando.objeto.buttons[1])) {
+			console.log("Mando: B");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[2])) {
+			console.log("Mando: X");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[3])) {
+			console.log("Mando: Y");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[4])) {
+			console.log("Mando: L1");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[5])) {
+			console.log("Mando: R1");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[6])) {
+			console.log("Mando: L2");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[7])) {
+			console.log("Mando: R2");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[8])) {
+			console.log("Mando: SELECT");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[9])) {
+			console.log("Mando: START");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[10])) {
+			console.log("Mando: L3");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[11])) {
+			console.log("Mando: R3");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[12])) {
+			console.log("Mando: Arriba");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[13])) {
+			console.log("Mando: Abajo");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[14])) {
+			console.log("Mando: Izquierda");
+		}
+		if (mando.botonPulsado(mando.objeto.buttons[15])) {
+			console.log("Mando: Derecha");
+		}
 	},
 	botonPulsado: function(boton) {
 		if (typeof(boton) == "object") {
