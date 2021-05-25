2aee3721ac556c97cc54b361ad01ec5958e99189
RSanchez98 <rsanchez98rsv@gmail.com>
2021-05-25 19:37:10 +0200

    Intento de inicio de sesion

--- Bowie's Game/index.php
+++ Bowie's Game/index.php
@@ -3,17 +3,24 @@
 <html lang="es">
 	<head>
 		<meta charset="utf-8">
-		<title>Juego web</title>
-		<meta name="description" conent="Esto es un juego web">
-		<meta name="author" conent="JavaDevOne">
+		<title>Bowie's Game!</title>
+		<meta name="description" conent="TFG JUEGO WEB PARA IES Romero Vargas">
+		<meta name="author" conent="Rodrigo Sánchez Valle">
+		<link rel ="icon" type="image/png" href="img/faces/base1.png"/>
 	</head>
+	<!-- <div>
+		<input type="text" id = "usuario"> <br>
+		<input type="text" id = "contraseña"><br>
+		<input type="submit" onclick="Grabar()" id="parrafo" value="Submit" style="color: white;">Guardar</input>
+	</div> -->
 	<body>
-		<div id="juego">
+	
+		<div id="juego">	
 			<div id="pantalla-titulo">
-			</div>
+			</div>	
 
 			<div id="mapa">
-			</div>
+			</div>		
 
 			<div id="jugador">
 			</div>
@@ -23,12 +30,55 @@
 
 			<div id="localizaciones">
 			</div>
-			
+
 			<div id="popup">
 			</div>
+
+			<div id="inventario">
+				<div id="cabecera">INVENTARIO</div>
+				<div id="contenido">Contenido</div>
+			</div>
+
+			<div id ="inicioSesion">
+				<form action ="#">
+					<label for = "usuario" style = "color: white;"> Usuario: </label>
+					<input type="text" id="usuario" name="usuario"><br>
+
+					<label for = "contraseña" style = "color: white;"> Contraseña: </label>
+					<input type="text" id="contraseña" name="contraseña"><br>
+					
+					<input onclick="Grabar()" type="submit" value="Submit">
+				</form>
+			</div>
+			
+
 		</div>
 		<?php
 			include_once 'app/cargadorArchivosJS.inc.php';
 		?>
+		<script>
+			function Grabar()
+			{
+				var usuario = document.getElementById("usuario").innerHTML;
+				var contraseña = document.getElementById("contraseña").innerHTML;
+
+				var xhr = XMLHttpRequest();
+				xhr.onredystatechange = function()
+				{
+					if(xhr.readyState == 4 && xhr.status == 200)
+					{
+						if(xhr.responseText == 'N')
+						{
+							alert('Error en la grabacion');
+						}
+					}
+				}
+
+				xhr.open("POST", "servidor/graba.php", true);
+				xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); 
+				xhr.send('usuario=${usuario}&contraseña=${contraseña}');
+			}
+		</script>
 	</body>
+	
 </html>
--- Bowie's Game/js/registroLocalizaciones.js
+++ Bowie's Game/js/registroLocalizaciones.js
@@ -2,8 +2,6 @@ var registroLocalizaciones = {
     obtenerLocalizacion: function(nombreLocalizacion) {
         let localizaciones = new Array();
         localizaciones.push(new RegistroLocalizacionEntrada("Ciudad del Arbol Milenario", "niveles/villa48.json", "img/villa48.nivel.png", 0, 630));
-        //localizaciones.push(new RegistroLocalizacionEntrada("Salida", "mapa/desierto48.json", "img/mapa48.mapa.png", 0, 100));
-
         for(var i = 0; i < localizaciones.length; i++) {
             if(nombreLocalizacion == localizaciones[i].nombre) {
                 return localizaciones[i];
