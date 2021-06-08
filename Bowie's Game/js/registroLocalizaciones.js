490f519d81d13daeff37630fae82b50f88930162
RSanchez98 <rsanchez98rsv@gmail.com>
2021-06-08 13:19:16 +0200

    Niveles nuevos

--- Bowie's Game/js/registroLocalizaciones.js
+++ Bowie's Game/js/registroLocalizaciones.js
@@ -3,6 +3,7 @@ var registroLocalizaciones = {
         let localizaciones = new Array();
         localizaciones.push(new RegistroLocalizacionEntrada("Ciudad del Arbol Milenario", "niveles/villa49.json", "img/villa49.nivel.png", 50, 1500)); 
         localizaciones.push(new RegistroLocalizacionEntrada("Pedrusco de la Rascacion", "niveles/villa48.json", "img/villa48.nivel.png", 200, 630));
+        localizaciones.push(new RegistroLocalizacionEntrada("La Camita Azul", "niveles/nivelGrande.json", "img/nivelGrande.nivel.png", 50, 400));
 
         for(var i = 0; i < localizaciones.length; i++) {
             if(nombreLocalizacion == localizaciones[i].nombre) {
---
+++ Bowie's Game/niveles/baseNivelGrande.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="baseNivelGrande" tilewidth="48" tileheight="48" tilecount="2006" columns="59">
+ <image source="../img/baseNivelGrande.png" width="2832" height="1632"/>
+</tileset>
